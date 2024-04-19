0371c471e0e6976af5ceaad4780404a75d7bd411
Nicholas <nicholasikiroma@gmail.com>
2024-04-19 22:08:31 +0100

    Initial commit

---
+++ tailgen/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for TailGen"""
+
+# tailgen/__init__.py
+
+__app_name__ = "tailgen"
+__version__ = "0.1.0"
---
+++ tailgen/__main__.py
@@ -0,0 +1,11 @@
+"""TailGen entry point script"""
+
+from tailgen import __app_name__
+
+
+def main():
+    cli.app(prog_name=__app_name__)
+
+
+if __name__ == "__main__":
+    main()
